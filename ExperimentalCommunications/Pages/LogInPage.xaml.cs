0d8c3ef63b1415978c2dc5cbeaf440da0ebb48af
Thatawesomeguy4 <derekteepee@gmail.com>
2021-07-10 11:38:11 -0400

    Created Log in Page

--- ExperimentalCommunications/Pages/LogInPage.xaml.cs
+++ ExperimentalCommunications/Pages/LogInPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,17 +11,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ExperimentalCommunications
+namespace ExperimentalCommunications.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LogInPage.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class LogInPage : Page
     {
-        public MainWindow()
+        public LogInPage()
         {
             InitializeComponent();
         }
-
     }
 }
